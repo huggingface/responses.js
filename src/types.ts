5166be3d6c63d7a94009e3d685d6b261a0c5e7d3
Wauplin <lucainp@gmail.com>
2025-07-08 15:31:18 +0200

    useless file

--- src/types.ts
+++
@@ -1,7 +0,0 @@
-import type { Client } from "@modelcontextprotocol/sdk/client/index.js";
-import type { ChatCompletionInputTool } from "@huggingface/tasks/dist/commonjs/tasks/chat-completion/inference.js";
-
-export interface McpClient {
-	client: Client;
-	tools: Record<string, ChatCompletionInputTool>;
-}
